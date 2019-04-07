7cca0736645b5037335b5de5a2f74ea94c3c8146
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 23:48:45 +0000

    Snapshot of branch dev taken on 2019-04-07T19:48:44.253-04:00 from "https://github.com/UnofficialJuliaMirror/LuxurySparse.jl-d05aeea4-b7d4-55ac-b691-9e7fabb07ba2"

---
+++ src/LuxurySparse.jl
@@ -0,0 +1,76 @@
+"""
+    LuxurySparse
+
+A luxury sparse matrix library for Julia.
+
+NOTE: this library may be a pirate for the following builtin array types:
+
+- `Diagonal`
+"""
+module LuxurySparse
+
+using Random, LinearAlgebra, SparseArrays
+
+# types that we will inherit from
+import SparseArrays: AbstractSparseArray, AbstractSparseMatrix
+
+# types will be overloaded by some pirate methods
+import LinearAlgebra: Diagonal, Transpose
+import SparseArrays: SparseMatrixCSC
+
+# APIs that we will overload for
+## standard array interface
+import Base: getindex, size, similar, copy, show, copyto!, inv, *, transpose
+
+## linear algebra
+import LinearAlgebra: ishermitian, issymmetric, diag, logdet, mul!
+export ishermitian, issymmetric
+
+## sparse arrays
+import SparseArrays: nnz, nonzeros, dropzeros!, issparse
+export nnz, nonzeros, dropzeros!, issparse
+
+################################################################################
+
+export isdense
+
+# additional traits
+"""
+    isdense(T) -> Bool
+    isdense(A) -> Bool
+
+Determine whether type `T` or array `A` is dense.
+"""
+function isdense end
+
+# subtype of DenseArray is dense by default
+isdense(::DenseArray) = true
+isdense(::Type{<:DenseArray}) = true
+
+isdense(::Type{<:AbstractSparseArray}) = false
+isdense(::AbstractSparseArray) = false
+
+# TODO: this need to be added to upstream
+isdense(::Diagonal) = false
+issparse(::Diagonal) = true
+
+abstract type AbstractLuxurySparseMatrix{Tv, Ti} <: AbstractSparseMatrix{Tv, Ti} end
+
+# IMatrix
+export IMatrix
+include("IMatrix.jl")
+
+# PermMatrix
+export PermMatrix
+include("PermMatrix.jl")
+
+include("conversions.jl")
+
+export pmrand
+include("random.jl")
+
+include("linalg.jl")
+include("matmul.jl")
+include("adjtrans.jl")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using LuxurySparse
+using Test
+
+include("IMatrix.jl")
+include("PermMatrix.jl")
